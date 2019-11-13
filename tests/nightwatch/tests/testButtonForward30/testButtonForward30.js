683e29ae82550fe0e9523c47234d1abae5a143c2
mhaba <mhc93.marcos@gmail.com>
2019-11-13 11:10:56 +0100

    Button Forward test first approach.

---
+++ tests/nightwatch/tests/testButtonForward30/testButtonForward30.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'Test Page Load' : function (browser) {
+      browser
+        .url("http://localhost:8000/player/index.html?id=video-remote-1")
+        .waitForElementVisible('body', 1000)
+        .assert.visible('.play-icon')
+        .click('.videoWrapper')
+        .waitForElementNotVisible('.play-icon')
+        .waitForElementVisible('.buttonPlugin.left.flexSkip_Forward_30')
+        .assert.visible('.buttonPlugin.left.flexSkip_Forward_30')
+        .click('.buttonPlugin.left.flexSkip_Forward_30')
+        .end();
+    }
+  };
+  
