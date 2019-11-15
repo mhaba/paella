5b8e5a653df5ee646b8bc59a900a7829ef3294f5
mhaba <mhc93.marcos@gmail.com>
2019-11-15 12:01:42 +0100

    Playback rate button test first approach.

---
+++ tests/nightwatch/tests/testPlaybackRateButton/testPlaybackRateButton.js
@@ -0,0 +1,25 @@
+module.exports = {
+    'Test Page Load' : function (browser) {
+      browser
+        .url("http://localhost:8000/player/index.html?id=video-remote-1")
+        .waitForElementVisible('body', 1000)
+        .assert.visible('.play-icon')
+        .click('.videoWrapper')
+        .waitForElementNotVisible('.play-icon')
+        .waitForElementVisible('.buttonPlugin.left.showPlaybackRateButton')
+        .assert.visible('.buttonPlugin.left.showPlaybackRateButton')
+        .click('.buttonPlugin.left.showPlaybackRateButton')
+        .waitForElementVisible('.buttonPluginPopUp.showPlaybackRateButton')
+
+        .useXpath()
+        .waitForElementVisible("//*[@id='buttonPlugin4_container']/div[1]")
+        .click("//*[@id='buttonPlugin4_container']/div[1]")
+        .useCss()
+
+        .assert.containsText("#buttonPlugin3 .button-text", "0.75x")
+
+        //Ver si la velocidad de reproducción pasa a 0.75
+
+        .end();
+    }
+  };
