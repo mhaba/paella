c1fdb0411539254d66812b5f6acb4224180c3c42
mhaba <mhc93.marcos@gmail.com>
2019-11-19 10:17:22 +0100

    Zoom button first approach test.

---
+++ tests/nightwatch/tests/testButtonZoom/testButtonZoom.js
@@ -0,0 +1,23 @@
+module.exports = {
+    'Test Page Load' : function (browser) {
+      browser
+        .url("http://localhost:8000/player/index.html?id=video-remote-1")
+        .waitForElementVisible('body', 1000)
+        .assert.visible('.play-icon')
+        .click('.videoWrapper')
+        .waitForElementNotVisible('.play-icon')
+
+        .waitForElementVisible('#buttonPlugin17')
+        .assert.visible('#buttonPlugin17')
+        .click('#buttonPlugin17')
+        .waitForElementVisible('#buttonPlugin18_container')
+        .assert.visible('#buttonPlugin18_container')
+
+        .assert.visible('.glyphicon.glyphicon-zoom-in')
+        .click('.glyphicon.glyphicon-zoom-in')
+
+        //Comprobar si se ha hecho zoom.
+
+        .end();
+    }
+  };
