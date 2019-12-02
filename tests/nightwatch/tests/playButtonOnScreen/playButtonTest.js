1bd74c63835f65f82111f527706ee587fc4dedd2
mhaba <mhc93.marcos@gmail.com>
2019-12-02 09:46:38 +0100

    no message

--- tests/nightwatch/tests/playButtonOnScreen/playButtonTest.js
+++ tests/nightwatch/tests/playButtonOnScreen/playButtonTest.js
@@ -4,7 +4,7 @@ module.exports = {
         .url("http://localhost:8000/player/index.html?id=video-remote-1")
         .waitForElementVisible('body', 1000)
         .assert.visible('.play-icon')
-        .click('.videoWrapper') //Es la div que recibe el click
+        .click('.videoWrapper')
         .waitForElementNotVisible('.play-icon')
         .assert.hidden('.play-icon')
         .end();
